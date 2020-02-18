d02897b00613d834c44cd6cb6ca815be74889116
acarlson98 <acarlson98@gmail.com>
2020-02-18 16:28:58 -0600

    trying to get data channel working

--- static/video_simple_hd.js
+++ static/video_simple_hd.js
@@ -1,43 +1,28 @@
-//
-//Copyright (c) 2016, Skedans Systems, Inc.
-//All rights reserved.
-//
-//Redistribution and use in source and binary forms, with or without
-//modification, are permitted provided that the following conditions are met:
-//
-//    * Redistributions of source code must retain the above copyright notice,
-//      this list of conditions and the following disclaimer.
-//    * Redistributions in binary form must reproduce the above copyright
-//      notice, this list of conditions and the following disclaimer in the
-//      documentation and/or other materials provided with the distribution.
-//
-//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-//AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-//IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-//ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-//LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-//CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-//SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-//INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-//CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-//ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-//POSSIBILITY OF SUCH DAMAGE.
-//
-// var easyrtc = require('open-easyrtc');
-
 var selfEasyrtcid = "";
 // var channelIsActive = {}; // tracks which channels are active
 
 
 function connect() {
     easyrtc.setVideoDims(1280,720);
-    //turn this off for production
-    easyrtc.enableDebug(true);
-    // easyrtc.setDataChannelOpenListener(openListener);
-    // easyrtc.setDataChannelCloseListener(closeListener);
+    easyrtc.enableDebug(true); //turn this off for production
+    easyrtc.enableDataChannels(true);
+    //not sure what these two do
+    easyrtc.setDataChannelOpenListener(openListener);
+    easyrtc.setDataChannelCloseListener(closeListener);
+
     // easyrtc.setPeerListener(addToConversation);
     easyrtc.setRoomOccupantListener(convertListToButtons);
+
+    /** Provides a layer on top of the easyrtc.initMediaSource and easyrtc.connect, assign the local media stream to
+     * the video object identified by monitorVideoId, assign remote video streams to
+     * the video objects identified by videoIds, and then call onReady. One of it's
+     * side effects is to add hangup buttons to the remote video objects, buttons
+     * that only appear when you hover over them with the mouse cursor. This method will also add the
+     * easyrtcMirror class to the monitor video object so that it behaves like a mirror.
+     */
     easyrtc.easyApp("easyrtc.videoChatHd", "selfVideo", ["callerVideo"], loginSuccess, loginFailure);
+    // Can't call connect again after already doing it above
+    // easyrtc.connect("easyrtc.dataMessaging", loginSuccess, loginFailure);
 }
 
 
@@ -50,16 +35,16 @@ function connect() {
 // }
 
 
-// function openListener(otherParty) {
-//     channelIsActive[otherParty] = true;
-//     updateButtonState(otherParty);
-// }
+function openListener(otherParty) {
+    channelIsActive[otherParty] = true;
+    updateButtonState(otherParty);
+}
 
 
-// function closeListener(otherParty) {
-//     channelIsActive[otherParty] = false;
-//     updateButtonState(otherParty);
-// }
+function closeListener(otherParty) {
+    channelIsActive[otherParty] = false;
+    updateButtonState(otherParty);
+}
 
 
 function clearConnectList() {
@@ -81,21 +66,21 @@ function clearConnectList() {
 // }
 
 
-// function sendStuffP2P(otherEasyrtcid) {
-//     var text = document.getElementById('sendMessageText').value;
-//     if (text.replace(/\s/g, "").length === 0) { // Don't send just whitespace
-//         return;
-//     }
-//     if (easyrtc.getConnectStatus(otherEasyrtcid) === easyrtc.IS_CONNECTED) {
-//         easyrtc.sendDataP2P(otherEasyrtcid, 'msg', text);
-//     }
-//     else {
-//         easyrtc.showError("NOT-CONNECTED", "not connected to " + easyrtc.idToName(otherEasyrtcid) + " yet.");
-//     }
+function sendStuffP2P(otherEasyrtcid) {
+    var text = document.getElementById('sendMessageText').value;
+    if (text.replace(/\s/g, "").length === 0) { // Don't send just whitespace
+        return;
+    }
+    if (easyrtc.getConnectStatus(otherEasyrtcid) === easyrtc.IS_CONNECTED) {
+        easyrtc.sendDataP2P(otherEasyrtcid, 'msg', text);
+    }
+    else {
+        easyrtc.showError("NOT-CONNECTED", "not connected to " + easyrtc.idToName(otherEasyrtcid) + " yet.");
+    }
 
-//     addToConversation("Me", "msgtype", text);
-//     document.getElementById('sendMessageText').value = "";
-// }
+    addToConversation("Me", "msgtype", text);
+    document.getElementById('sendMessageText').value = "";
+}
 
 
 function convertListToButtons (roomName, data, isPrimary) {
