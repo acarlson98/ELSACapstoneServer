300a04bcef69d37fccd7bbfbc79980e4fbd190de
acarlson98 <acarlson98@gmail.com>
2020-02-25 01:10:19 -0600

    pulling over work on server

--- static/video_simple_hd.js
+++ static/video_simple_hd.js
@@ -4,8 +4,7 @@ var channelIsActive = {}; // tracks which channels are active
 
 function connect() {
     easyrtc.setVideoDims(1280,720);
-    // easyrtc.enableDebug(true); //turn this off for production
-    // easyrtc.logLevel('debug');
+    easyrtc.enableDebug(true); //turn this off for production
     easyrtc.enableDataChannels(true);
 
     //not sure what these four do
