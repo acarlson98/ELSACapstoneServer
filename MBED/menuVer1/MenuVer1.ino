db8defee09fd811f8cb2e5ac4645274d19cfdca9
Takumi Ito <nekotakumi@gmail.com>
2020-03-05 13:50:36 -0600

    update

--- MBED/menuVer1/MenuVer1.ino
+++ MBED/menuVer1/MenuVer1.ino
@@ -1,4 +1,3 @@
-
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -7,9 +6,6 @@
 #include "ThingSpeak.h" //install library for thing speak
 #include <WiFi.h>
 
-#include "MAX30105.h"
-#include "heartRate.h"
-
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
@@ -17,7 +13,7 @@ int menunum = 4;
 int menumod = 4;
 int const tact1 = 16;
 int const tact2 = 17;
-int const tact3 = 18;
+int const tact3 = 35;
 
 //Thing Speak neededd
 char ssid[] = "PKI";        // your network SSID (name) 
@@ -29,17 +25,10 @@ const char * myWriteAPIKey = "77PYOKK03FXL15CW";
 // Initialize our values
 String myStatus = "";
 
-int BPM = 0;
-int O2  = 0;
-int HTemp = 0;
+int BPM;
+int O2;
+int HTemp;
 
-MAX30105 particleSensor;
-const byte RATE_SIZE = 6; //Increase this for more averaging. 4 is good.
-byte rates[RATE_SIZE]; //Array of heart rates
-byte rateSpot = 0;
-long lastBeat = 0; //Time at which the last beat occurred
-float beatsPerMinute;
-int beatAvg;
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
@@ -245,8 +234,7 @@ void sendHeart(){
 
   // set the fields with the values
   ThingSpeak.setField(1, BPM);
-  ThingSpeak.setField(1, O2);
-  ThingSpeak.setField(1, HTemp);
+
   
   // set the status
   ThingSpeak.setStatus(myStatus);
@@ -260,59 +248,6 @@ void sendHeart(){
     Serial.println("Problem updating channel. HTTP error code " + String(x));
   }
 }
-void readHeart(){
- // Initialize sensor
-  if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
-  {
-    Serial.println("MAX30105 was not found. Please check wiring/power. ");
-    while (1);
-  }
-  Serial.println("Place your index finger on the sensor with steady pressure.");
-
-  particleSensor.setup(); //Configure sensor with default settings
-  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
-  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
-  int countmonitor = 1000;
-  while(countmonitor>0){
-        long irValue = particleSensor.getIR();
-
-  if (checkForBeat(irValue) == true)
-  {
-    //We sensed a beat!
-    long delta = millis() - lastBeat;
-    lastBeat = millis();
-
-    beatsPerMinute = 60 / (delta / 1000.0);
-
-    if (beatsPerMinute < 255 && beatsPerMinute > 20)
-    {
-      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-      rateSpot %= RATE_SIZE; //Wrap variable
-
-      //Take average of readings
-      beatAvg = 0;
-      for (byte x = 0 ; x < RATE_SIZE ; x++)
-        beatAvg += rates[x];
-      beatAvg /= RATE_SIZE;
-    }
-  }
-
-  Serial.print("IR=");
-  Serial.print(irValue);
-  Serial.print(", BPM=");
-  Serial.print(beatsPerMinute);
-  Serial.print(", Avg BPM=");
-  Serial.print(beatAvg);
-
-  if (irValue < 50000)
-    Serial.print(" No finger?");
-
-  Serial.println();
-  countmonitor--;
-  }
-  return;
-}
-
 void setup() {
   Serial.begin(115200);  //Initialize serial
   WiFi.mode(WIFI_STA);   
