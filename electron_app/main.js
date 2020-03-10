2eaf340ae67466654eab8011ecef9b902c2817b7
acarlson98 <acarlson98@gmail.com>
2020-03-09 19:20:27 -0500

    Add webview tag, might work? need to re-add onoff

--- electron_app/main.js
+++ electron_app/main.js
@@ -1,11 +1,12 @@
 const { app, BrowserWindow } = require('electron');
-const Gpio = require('onoff').Gpio;
+// const Gpio = require('onoff').Gpio;
 
 app.on('ready', function() {
     var mainWindow = new BrowserWindow({
 		show: false,
 		webPreferences: {
 			nodeIntegration: true,
+			webviewTag: true,
 		}
     });
     mainWindow.maximize();
@@ -13,21 +14,21 @@ app.on('ready', function() {
 	mainWindow.webContents.openDevTools(); // Debug
 	mainWindow.show();
 	
-	const led = new Gpio(17, 'out');
-	const button = new Gpio(4, 'in', 'both');
+	// const led = new Gpio(17, 'out');
+	// const button = new Gpio(4, 'in', 'both');
 		
-	button.watch((err, value) => {
-		if (err) {
-		throw err;
-		}
+	// button.watch((err, value) => {
+	// 	if (err) {
+	// 	throw err;
+	// 	}
 		
-		led.writeSync(value);
-		console.log("Button Status: " + value); //Debug
-		mainWindow.webContents.send('button-status', value);
-	});
+	// 	led.writeSync(value);
+	// 	console.log("Button Status: " + value); //Debug
+	// 	mainWindow.webContents.send('button-status', value);
+	// });
 		
-	process.on('SIGINT', _ => {
-		led.unexport();
-		button.unexport();
-	});
+	// process.on('SIGINT', _ => {
+	// 	led.unexport();
+	// 	button.unexport();
+	// });
 });
